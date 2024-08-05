557f7c3fa5446a7246a45e4502d95b0c2c4c748b
kandyy9 <a.kandy.sk@gmail.com>
2024-08-05 18:55:33 +0300

    added folder and updated paths

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,10 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
+import reactLogo from "../assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 
-function App() {
+export default function App() {
   const [count, setCount] = useState(0);
-
   return (
     <>
       <div>
@@ -31,5 +30,3 @@ function App() {
     </>
   );
 }
-
-export default App;
